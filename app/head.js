c9350e18ed68ec32c4381a9b6c8c283a04010b1b
Gary Meehan <garymeehan@outlook.com>
2022-10-29 16:45:09 +0100

    add example

---
+++ app/head.js
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Home</title>
+      <meta name="description" content="Home" key="description" />
+    </>
+  );
+}
---
+++ app/one/two/head.js
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Two</title>
+      <meta name="description" content="Two" key="description" />
+    </>
+  );
+}
