a03557c8eac9d78b1986855d023c63cc9da60b97
Gary Meehan <garymeehan@outlook.com>
2022-10-30 12:37:39 +0000

    add to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head></head>
+      <head>
+        <meta name="other" content="I'm in layout.js" key="other" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/one/two/head.js
+++ app/one/two/head.js
@@ -3,6 +3,11 @@ export default function Head() {
     <>
       <title>Two</title>
       <meta name="description" content="Two" key="description" />
+      <meta
+        name="other"
+        content="I thought I would override layout.js version"
+        key="other"
+      />
     </>
   );
 }
